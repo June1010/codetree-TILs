a676fca615c1a7b00f1812bdaa0dc5cb63f3f5c0
June1010 <61878436+June1010@users.noreply.github.com>
2023-11-28 15:35:41 +0900

    Title: 가장 짧은 부분합, Time: 199ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 231128/가장 짧은 부분합/shortest-subtotal.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.stream.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] str = br.readLine().split(" ");
+        int N = Integer.parseInt(str[0]);
+        int M = Integer.parseInt(str[1]);
+        int ans = 999_999;
+
+        int[] arr = Arrays.stream(br.readLine().split(" "))
+          .mapToInt(Integer::parseInt)
+          .toArray();
+
+        int sum = 0;
+        int j = 0;
+        for (int i = 0; i < N; i++) {
+            while (j < N && sum + arr[j] <= M) {
+                sum += arr[j];
+                j++;
+            }
+
+            ans = Math.min(ans, j-i+1);
+            sum -= arr[i];
+        }
+
+        ans = (ans == 999_999) ? -1 : ans;
+        System.out.println(ans);
+    }
+}
